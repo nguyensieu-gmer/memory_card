f1804933a54c8abebf1927e0afda090986bab54c
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-07-13 09:44:03 +0700

    Add logic for layout Render

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { List } from "./component/list";
+import { Guide } from "./component/guide";
+import { Score } from "./component/score";
+import radomPokemon from "./component/randomListOfPokemon.js";
+import { Level } from "./component/gameLevel.jsx";
 
 function App() {
-  return (
-    <>
-      <List />
-    </>
+  const [level, setLevel] = useState(5);
+  const [gamePlay, setGamePlay] = useState(false);
+  const randomList = radomPokemon(level);
+  const mainComponent = (
+    <main>
+      <header>
+        <Guide /> <Score />
+      </header>
+      <section>
+        <List randomList={randomList} />
+      </section>
+    </main>
+  );
+  return gamePlay ? (
+    mainComponent
+  ) : (
+    <Level setLevel={setLevel} setGamePlay={setGamePlay} />
   );
 }
 
