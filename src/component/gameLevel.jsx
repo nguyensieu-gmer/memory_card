d975c5c4f25c95ec2a202e37042cd41be3c0f49c
Nguyen Van Sieu <217349416+nguyensieu-gmer@users.noreply.github.com>
2026-07-14 11:26:02 +0700

    Add title

--- src/component/gameLevel.jsx
+++ src/component/gameLevel.jsx
@@ -1,4 +1,5 @@
 import radomPokemon from "./randomListOfPokemon.js";
+import "../style/level.css";
 
 export function Level({ setRandomList, setGamePlay }) {
   function handleClick(e) {
@@ -7,16 +8,19 @@ export function Level({ setRandomList, setGamePlay }) {
     setGamePlay(true);
   }
   return (
-    <div className="level_buttons">
-      <button value={5} onClick={handleClick}>
-        Easy
-      </button>
-      <button value={10} onClick={handleClick}>
-        Medium
-      </button>
-      <button value={20} onClick={handleClick}>
-        Hard
-      </button>
+    <div className="level">
+      <h1>Please choose your level game</h1>
+      <div className="level_buttons">
+        <button value={5} onClick={handleClick}>
+          Easy
+        </button>
+        <button value={10} onClick={handleClick}>
+          Medium
+        </button>
+        <button value={20} onClick={handleClick}>
+          Hard
+        </button>
+      </div>
     </div>
   );
 }
